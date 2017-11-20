c4f23d599827a5fcd447e8009905f9d25f3d2f99
KRISHINC\milap.patel <markpatel.java@gmail.com>
2017-11-20 12:58:13 +0530

    TSHOP Accelerator [ant modulegen accelerator tshop com.tshop] : Initialized : ant clean

---
+++ tshop/tshopcore/src/com/tshop/core/constants/TshopCoreConstants.java
@@ -0,0 +1,27 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2000-2016 SAP SE or an SAP affiliate company.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of SAP
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with SAP.
+ */
+package com.tshop.core.constants;
+
+/**
+ * Global class for all TshopCore constants. You can add global constants for your extension into this class.
+ */
+public final class TshopCoreConstants extends GeneratedTshopCoreConstants
+{
+	public static final String EXTENSIONNAME = "tshopcore";
+
+	private TshopCoreConstants()
+	{
+		//empty
+	}
+
+	// implement here constants used by this extension
+}
---
+++ tshop/tshopinitialdata/src/com/tshop/initialdata/constants/TshopInitialDataConstants.java
@@ -0,0 +1,25 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2000-2016 SAP SE or an SAP affiliate company.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of SAP
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with SAP.
+ */
+package com.tshop.initialdata.constants;
+
+/**
+ * Global class for all TshopInitialData constants.
+ */
+public final class TshopInitialDataConstants extends GeneratedTshopInitialDataConstants
+{
+	public static final String EXTENSIONNAME = "tshopinitialdata";
+
+	private TshopInitialDataConstants()
+	{
+		//empty
+	}
+}
